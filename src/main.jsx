57fc81e98c187c97a18576cccd1c2e58fb0162bd
Mustafa <mustafaprogrammer786@gmail.com>
2026-04-02 11:04:45 -0700

    Fix the consitent issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Atelier from './Atelier.jsx'
-import OceanLivingCertification from './OceanLivingCertification';
-import CheckoutPage             from './CheckoutPage.jsx';
-import ThankYouPage             from './ThankYouPage';
-import CoursePage               from './CoursePage';
-
+import OceanLivingCertification from './OceanLivingCertification.jsx'
+import CheckoutPage from './CheckoutPage.jsx'
+import ThankYouPage from './ThankYouPage.jsx'
+import CoursePage from './CoursePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,9 +16,9 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/atelier" element={<Atelier />} />
         <Route path="/ocean-living-certification" element={<OceanLivingCertification />} />
-        <Route path="/checkout-ocean-living"      element={<CheckoutPage />} />
-<Route path="/thank-you-ocean"            element={<ThankYouPage />} />
-<Route path="/course-ocean-living"        element={<CoursePage />}/>
+        <Route path="/checkout-ocean-living" element={<CheckoutPage />} />
+        <Route path="/thank-you-ocean" element={<ThankYouPage />} />
+        <Route path="/course-ocean-living" element={<CoursePage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
